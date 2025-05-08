d62b1b4200666d5c730638c3121d40c86b713c66
Carlos Arizpe <arizpehernandezcarlos@johndeere.com>
2025-05-08 13:17:57 -0600

    chore :wrench: | fix home path on router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from '@/pages/home/Home'
-import { Login } from '@/pages/login/Login'
+import { Routes, Route } from "react-router-dom";
+import { Home } from "@/pages/home/home";
+import { Login } from "@/pages/login/Login";
 
 export const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
+  );
+};
